cb95f7689e43b8747c926d9d7f0b673e69fd1671
RefaelAharouni <refael.aharouni@edu.devinci.fr>
2025-12-31 12:28:42 +0100

    Rename the number of the SCRUM task from 4 to 13

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 import { request } from 'supertest'
 import app from '../src/app.js'
 
-describe('SCRUM-4-schedule-training-session', () => {
+describe('SCRUM-13-schedule-training-session', () => {
   it('returns the free facilities', async () => {
     const response = await request(app).get('/trainings/available-facilities')
       .query({ date_event: '2024-09-02', slot_event: '18:00-20:00' })
