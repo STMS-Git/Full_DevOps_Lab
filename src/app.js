f0862af960ea8b838a81e6c5fd34d703c055af40
RefaelAharouni <refael.aharouni@edu.devinci.fr>
2026-01-01 23:12:06 +0100

    feat(SCRUM-51): schedule match session Creation of the match session for the coaches only Check of the availability Error handling (when there is no facility available) Integration of tests

--- src/app.js
+++ src/app.js
@@ -8,21 +8,42 @@
 import express from 'express'
 import { errorHandler } from './utils/errorHandler.js'
 
-// Import des routes
+// Import of the routes
 import versionRoute from './routes/auto/version.route.js'
 import infoRoute from './routes/auto/info.route.js'// À décommenter plus tard
 import boomRoute from './routes/auto/boom.route.js'// À décommenter plus tard
+import matchRoute from './routes/auto/match.route.js'
 
 const app = express()
 
+// JSON Middlewares
+app.use(express.json())
+
+app.use((req, _res, next) => {
+  const authentificationuser = req.headers.authorization || ''
+  if (authentificationuser === 'Bearer coach-token') {
+    req.user = { id: 1, role: 'coach' }
+  } else if (authentificationuser === 'Bearer player-token') {
+    req.user = { id: 2, role: 'player' }
+  } else {
+    req.user = null
+  }
+  next()
+})
+
 // Simple root + health endpoints
-app.get('/', (_req, res) => res.json({ ok: true, message: 'Hello from CI/CD demo 👋' }))
-app.get('/health', (_req, res) => res.status(200).send('OK'))
+app.get('/', (req, res) => {
+  res.json({ ok: true, message: 'Hello from CI/CD demo' })
+})
+app.get('/health', (req, res) => {
+  res.status(200).send('OK')
+})
 
 // Connecter les routes
 app.use('/version', versionRoute)
 app.use('/info', infoRoute) // À décommenter plus tard
 app.use('/boom', boomRoute) // À décommenter plus tard
+app.use('/matches', matchRoute)
 
 // Global error middleware last
 app.use(errorHandler)
