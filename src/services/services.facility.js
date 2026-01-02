8dda6b77e43923ca367e33bdffa70b74536fec8d
RefaelAharouni <refael.aharouni@edu.devinci.fr>
2026-01-02 14:13:45 +0100

    Delete src/services/services.facility.js (useless)

--- src/services/services.facility.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable camelcase */
-import { facilities } from '../data/facilities'
-
-export function findAllAvailableFacilities (list_events, slot, date) {
-  const IDsbusyFacilities = list_events.filter(e => e.slot_event === slot && e.date_event === date).map(e => e.facilityID_event)
-  return facilities.filter(f => !IDsbusyFacilities.includes(f.id))
-}
